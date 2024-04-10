5d36a243a6d56def845d5aceafb7ddf2f98b0da1
mayronmiguel <164046561+mayronmiguel@users.noreply.github.com>
2024-04-10 10:11:16 -0300

    Add files via upload

---
+++ Convertendo Valores/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String titulo=  "DesafioTemperatura";
+        System.out.println(titulo);
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+
+
+    }
+}
